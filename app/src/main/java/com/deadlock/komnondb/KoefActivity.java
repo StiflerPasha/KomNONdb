f48e6009796d529fa225966fdb80d15fcdaaf686
StiflerPasha <dvoynospavel@gmail.com>
2018-08-30 18:12:59 +0300

    add activity options

---
+++ app/src/main/java/com/deadlock/komnondb/KoefActivity.java
@@ -0,0 +1,13 @@
+package com.deadlock.komnondb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KoefActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_koef);
+    }
+}
